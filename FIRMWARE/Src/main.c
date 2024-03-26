77958cae0412ba6e3d805db8faf3ae7faf7e6f5b
slemanz <william_sleman@hotmail.com>
2024-03-26 03:23:50 -0300

    project(feat): add firmware

---
+++ FIRMWARE/Src/main.c
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * @author         : William Sleman - visit www.slemanz.com
+ * @brief          : Main program body
+ ******************************************************************************/
+
+#include "stm32f4.h"
+
+int main(void)
+{
+    /* Loop forever */
+	for(;;);
+}
