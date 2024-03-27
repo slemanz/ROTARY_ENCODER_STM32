a54a12c8bf69a376bd63fa9def03472a87d51060
slemanz <william_sleman@hotmail.com>
2024-03-27 04:29:12 -0300

    firmware(feat): add encoder value logiv

--- FIRMWARE/Src/main.c
+++ FIRMWARE/Src/main.c
@@ -13,21 +13,40 @@ int main(void)
 {
 	pinout_init();
 
-	int16_t pos_encoder = encoder_read();
+	int16_t pos = encoder_read(), pos_before = encoder_read(), diff, value;
+
+
 
 	while(1)
 	{
-		pos_encoder = encoder_read();
-		if(pos_encoder == 0)
+		pos = encoder_read();
+		diff = pos_before - pos;
+
+		 // turned clockwise
+		if (((diff == -1) || (diff == 3))) {
+			pos_before = pos;
+			if (value < 20) { value++; }
+
+		// turned counter-clockwise
+		} else if (((diff == 1) || (diff == -3))) {
+			pos_before = pos;
+			if (value > 0) { value--; }
+
+		// step missed
+		} else if ((diff == 2) || (diff == -2)) {
+
+		}
+
+		if((value/4) == 0)
 		{
 			LED1 = 1; LED2 = 0; LED3 = 0; LED4 = 0;
-		}else if(pos_encoder == 1)
+		}else if((value/4) == 1)
 		{
 			LED1 = 0; LED2 = 1; LED3 = 0; LED4 = 0;
-		}else if(pos_encoder == 2)
+		}else if((value/4) == 2)
 		{
 			LED1 = 0; LED2 = 0; LED3 = 1; LED4 = 0;
-		}else if(pos_encoder == 3)
+		}else if((value/4) == 3)
 		{
 			LED1 = 0; LED2 = 0; LED3 = 0; LED4 = 1;
 		}
