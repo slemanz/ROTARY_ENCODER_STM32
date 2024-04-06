c05e24eb5b956f4f31d4b473c63706664b609b9c
slemanz <william_sleman@hotmail.com>
2024-04-06 16:44:10 -0300

    project(create): create FIRMWARE V1

---
+++ FIRMWARE_V1/Drivers/Src/gpio.c
@@ -0,0 +1,257 @@
+/*
+ * gpio.c
+ *
+ *  Created on: Mar 26, 2024
+ *      Author: sleman
+ */
+
+/*
+ *  Peripheral clock setup
+ */
+
+/**************************************************************************
+ * @fn				- GPIO_PeriClockControl
+ *
+ * @brief			- this functions enable and disables peripheral clock
+ * 					  for the given GPIO port
+ *
+ * @param[in]		- base address of the gpio peripheral
+ * @param[in]		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ *
+ */
+
+#include "stm32f4.h"
+
+
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		if		(pGPIOx == GPIOA) GPIOA_PCLK_EN();
+		else if	(pGPIOx == GPIOB) GPIOB_PCLK_EN();
+		else if	(pGPIOx == GPIOC) GPIOC_PCLK_EN();
+		else if	(pGPIOx == GPIOD) GPIOD_PCLK_EN();
+		else if	(pGPIOx == GPIOE) GPIOE_PCLK_EN();
+		else if	(pGPIOx == GPIOH) GPIOH_PCLK_EN();
+	}else
+	{
+		// TODO
+	}
+}
+
+
+
+/*
+ * Init and De-init
+ */
+
+/**************************************************************************
+ * @fn				- GPIO_Init
+ *
+ * @brief			- this function config the given GPIO pin handle
+ *
+ * @param[in]		- GPIO handle struct
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ *
+ */
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
+{
+	uint32_t temp = 0; // temporary register
+
+	// 1. configure the mode of the gpio pin
+
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG)
+	{
+		temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+		pGPIOHandle->pGPIOx->MODER &= ~(0x3 << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber)); //clearing
+		pGPIOHandle->pGPIOx->MODER |= temp; // setting
+	}else
+	{
+		//1.2 configure interrupt mode
+		// not use in this application
+	}
+
+	temp = 0;
+
+	// 2. configure the speed
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinSpeed << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->OSPEEDR &= ~(0x3 << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->OSPEEDR |= temp;
+	temp = 0;
+
+
+	// 3. configure the pupd settings
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinPuPdControl << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->PUPDR &= ~(0x3 << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->PUPDR |= temp;
+	temp = 0;
+
+
+	// 4. configure the output type
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinOPType << (pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->OTYPER &= ~(0x1 << (pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->OTYPER |= temp;
+	temp = 0;
+
+
+	// 5. configure the alt functionality
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_ALTFN)
+	{
+		uint32_t temp1, temp2;
+
+		temp1 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 8;
+		temp2 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber % 8;
+		pGPIOHandle->pGPIOx->AFR[temp1] &= ~(0x0F << (4*temp2));
+		pGPIOHandle->pGPIOx->AFR[temp1] |= (pGPIOHandle->GPIO_PinConfig.GPIO_PinAltFunMode << (4*temp2));
+	}
+}
+
+
+
+/**************************************************************************
+ * @fn				- GPIO_DeInit
+ *
+ * @brief			- de init a port of GPIO
+ *
+ * @param[in]		- base address of gpio peripheral
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ *
+ */
+
+void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
+{
+	if		(pGPIOx == GPIOA) GPIOA_REG_RESET();
+	else if	(pGPIOx == GPIOB) GPIOB_REG_RESET();
+	else if	(pGPIOx == GPIOC) GPIOC_REG_RESET();
+	else if	(pGPIOx == GPIOD) GPIOD_REG_RESET();
+	else if	(pGPIOx == GPIOE) GPIOE_REG_RESET();
+	else if	(pGPIOx == GPIOH) GPIOH_REG_RESET();
+}
+
+
+
+
+/**************************************************************************
+ * @fn				- GPIO_ReadFromInputPin
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ *
+ */
+
+uint8_t  GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
+{
+	uint8_t value;
+	value = (uint8_t)((pGPIOx->IDR >> PinNumber) & 0x01);
+
+	return value;
+}
+
+
+
+/**************************************************************************
+ * @fn				- GPIO_ReadFromInputPort
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ *
+ */
+
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx)
+{
+	uint16_t value;
+	value = (uint16_t)(pGPIOx->IDR);
+
+	return value;
+}
+
+
+
+/**************************************************************************
+ * @fn				- GPIO_WriteToOutputPin
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ *
+ */
+
+void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Value)
+{
+	if(Value == GPIO_PIN_SET)
+	{
+		// write 1 to the output data register at the bitfield corresponding to the pin number
+		pGPIOx->ODR |= (1 << PinNumber);
+	}else
+	{
+		// write 0
+		pGPIOx->ODR &= ~(1 << PinNumber);
+	}
+}
+
+
+
+
+/**************************************************************************
+ * @fn				- GPIO_WriteToOutputPort
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ *
+ */
+
+
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value)
+{
+	pGPIOx->ODR = Value;
+}
+
+
+
+
+/**************************************************************************
+ * @fn				- GPIO_ToggleOutputPin
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ *
+ */
+
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
+{
+	pGPIOx->ODR ^= (1 << PinNumber);
+}
---
+++ FIRMWARE_V1/Src/main.c
@@ -0,0 +1,8 @@
+#include "stm32f4.h"
+
+
+int main(void)
+{
+    /* Loop forever */
+	for(;;);
+}
