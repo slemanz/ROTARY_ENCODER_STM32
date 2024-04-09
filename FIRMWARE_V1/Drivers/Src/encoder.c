e5e8bb1ae4240ec7c300b16e7e138dd0f8ac24b6
slemanz <william_sleman@hotmail.com>
2024-04-09 04:51:39 -0300

    firmware(test): test firmware to counter bcd - working fine

--- FIRMWARE_V1/Drivers/Src/encoder.c
+++ FIRMWARE_V1/Drivers/Src/encoder.c
@@ -2,12 +2,12 @@
 
 void encoder_counterSet(int16_t value)
 {
-	//uint32_t mask = 1U;
+	uint32_t mask = 1U;
 	if(value < 10 && value >= 0)
 	{
-		/*PIN_OUT1 = (value | ~mask);
-		PIN_OUT2 = ((value >> 1) | ~mask);
-		PIN_OUT3 = ((value >> 2) | ~mask);
-		PIN_OUT4 = ((value >> 3) | ~mask);*/
+		D0 = (value | ~mask);
+		D1 = ((value >> 1) | ~mask);
+		D2 = ((value >> 2) | ~mask);
+		D3 = ((value >> 3) | ~mask);
 	}
 }
--- FIRMWARE_V1/Src/main.c
+++ FIRMWARE_V1/Src/main.c
@@ -1,17 +1,25 @@
 #include "stm32f4.h"
 
+void delay(void)
+{
+	for(uint32_t i; i < 500000; i++);
+}
 
 int main(void)
 {
 	pinout_init();
-	D0 = 1;
-	D1 = 1;
-	D2 = 1;
+	D0 = 0;
+	D1 = 0;
+	D2 = 0;
 	D3 = 0;
 
 
 	while(1)
 	{
-
+		for(uint16_t i = 0; i < 10; i++)
+		{
+			encoder_counterSet(i);
+			delay();
+		}
 	}
 }
